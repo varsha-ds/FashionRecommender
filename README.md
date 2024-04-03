3c7eb1b3c08b821702b321e2cd09915c030517a4
Varsha-Dundhigalla <varshasfd@gmail.com>
2024-04-02 23:18:39 -0400

    built the model

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 CNN - Resnet (trained on Images)/ customized CNN model building
 using keras
+
 Transfer Learning
 
 
@@ -34,3 +35,36 @@ To Summarize, our model calculates the euclidean distance between the new vector
 Embeddings- Representing every image in 2048dimensions
 
 
+
+
+import pickle
+
+model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
+
+dummy_input = tensorflow.zeros((1, 224, 224, 3))
+
+# Build the model
+model.build(input_shape=dummy_input.shape)
+
+# Print model summary
+print(model.summary())
+
+keras works with bathes of images, even if we have only one image we have to specify that we have 1 batch with 1 image, for this the image needs to use expand dimensions funtion.
+
+preprocess_input= formats the given data according to the Imagenet Dataset format
+"RGB--> BGR
+each color channel is zero-centered with respect to the ImageNet dataset, without scaling."
+
+flatten is used to create 1d arrays from higher dimension
+
+Normalize(l2 Norm) = every number in the array divided by (sqroot(x1sq+x2sq...x2048sq))
+
+
+Note: MAin Dataset consists of 44k images but for this code, we only considered a sample with less memory of it provided by the dataset owner with the dataset link as given below:
+https://www.kaggle.com/datasets/paramaggarwal/fashion-product-images-dataset
--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
 import numpy as np
 from numpy.linalg import norm
 import os
-#from tqdm import tqdm
+from tqdm import tqdm
 import ssl
+import pickle
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -20,4 +21,37 @@ model = tensorflow.keras.Sequential([
     GlobalMaxPooling2D()
 ])
 
-print(model.summary())
+#dummy_input = tensorflow.zeros((1, 224, 224, 3))
+
+# Build the model
+#model.build(input_shape=dummy_input.shape)
+
+# Print model summary
+#print(model.summary())
+
+def extract_features(img_path,model):
+    img = image.load_img(img_path, target_size = (224,224))
+    img_array = image.img_to_array(img)
+    expand_img_array = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expand_img_array)
+    result = model.predict(preprocessed_img).flatten()
+    normalized_result = result/norm(result)
+    return normalized_result
+
+#creating a python list to store all the image names
+#print(os.listdir('images'))
+file_names = []
+
+for file in os.listdir('images'):
+    file_names.append(os.path.join('images',file))
+ 
+#we have the path for all the images, now for every file I have to call the function that returns the extracted features of the file.
+
+feature_list = []  #2D list of features, every list has 2048 features, one for each image
+for file in file_names:
+    feature_list.append(extract_features(file,model))
+
+#print(np.array(feature_list).shape)
+    
+pickle.dump(feature_list, open('embeddings.pkl','wb'))
+pickle.dump(file_names, open('filenames.pkl','wb'))
